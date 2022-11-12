8fb0e02c623b98b098c15d902d80965b36774a57
gurmukhsingh <gurmukhs300@gmail.com>
2022-11-12 15:36:00 +1100

    review adding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
+  resources :reviews, only: [:destroy]
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+50.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name,
+                              address: Faker::Address.full_address,
+                              phone_number: Faker::Number.number(digits: 10),
+                              category: ["Chinese", "Italian", "Japanese", "French", "Belgian"].sample
+                            )
+  restaurant.save
+  review = Review.new(rating: rand(0..5),
+                      content: Faker::Restaurant.review)
+  review.save
+end
+puts "all done"
